361f86b40ec395493531079de46c89b73350fc90
Marc Christensen <mecworks@gmail.com>
2026-02-13 23:37:00 -0700

    Add key_lookup struct and update KEYLIST_SIZE

--- keys.h
+++ keys.h
@@ -1,2 +1,13 @@
-// Updated KEYLIST_SIZE from 100 to 200
+struct key_lookup
+{
+    char name[20];
+    int code;
+};
+
 #define KEYLIST_SIZE 200
+
+struct key_lookup create_key (char* name, int code);
+void fill_keylist();
+int find_key (char* name);
+
+extern struct key_lookup keys[KEYLIST_SIZE];
