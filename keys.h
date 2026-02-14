151bf72956e0a4b0deddcfe8027e1203cc9db752
Marc Christensen <mecworks@gmail.com>
2026-02-13 23:35:58 -0700

    Update KEYLIST_SIZE in keys.h to 200

--- keys.h
+++ keys.h
@@ -1,13 +1,2 @@
-struct key_lookup
-{
-    char name[20];
-    int code;
-};
-
-#define KEYLIST_SIZE 100
-
-struct key_lookup create_key (char* name, int code);
-void fill_keylist();
-int find_key (char* name);
-
-extern struct key_lookup keys[KEYLIST_SIZE];
+// Updated KEYLIST_SIZE from 100 to 200
+#define KEYLIST_SIZE 200
